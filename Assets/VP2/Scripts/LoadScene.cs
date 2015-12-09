598c7b88fe30c5e994da20cf8dd8da3b312982e2
Bruno Braga Chagas <bbchagas@gmail.com>
2015-12-09 01:21:20 -0300

    Adicionado movimento, scprit para mudar de cena e alguns objetos nos cenários

---
+++ Assets/VP2/Scripts/LoadScene.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public Transform Target;
+    public float minDis;
+    public string Scene;
+    public int NumeroCena;
+
+    // Use this for initialization
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, Target.position) <= (minDis + 1))
+        {
+            if (NumeroCena != 0)
+            {
+                Application.LoadLevel(NumeroCena);
+            }
+            else
+                Application.LoadLevel(Scene);
+        }
+    }   
+}
